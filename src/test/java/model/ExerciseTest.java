19ce847f2544191663afd6da7978e26f96640fa4
Esteban Puyana <puyanasalazar.e@northeastern.edu>
2024-12-24 18:46:04 -0500

    finished implementation of exercise class and testing. Reached 95%/98% method/line coverage

--- src/test/java/model/ExerciseTest.java
+++ src/test/java/model/ExerciseTest.java
@@ -3,6 +3,8 @@ package model;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -46,6 +48,49 @@ public class ExerciseTest {
     }
 
     @Test
+    public void printExerciseOutputsCorrectFormat() {
+
+        Map<Integer, Integer> repsPerSet = new HashMap<>();
+        repsPerSet.put(0, 8);
+        repsPerSet.put(1, 8);
+        repsPerSet.put(2, 8);
+        repsPerSet.put(3, 8);
+
+        Exercise exercise = new Exercise("Shoulder Press", 4, repsPerSet, 8, 50.0, Mode.DUMBBELL);
+
+        // Redirect System.out to a ByteArrayOutputStream
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        exercise.printExercise();
+
+        // Reset System.out
+        System.setOut(System.out);
+
+        String expectedOutput = "Shoulder Press 4x8@50.0 (Reps per set: {0=8, 1=8, 2=8, 3=8})\n";
+        Assert.assertEquals(expectedOutput, outputStream.toString());
+    }
+
+    @Test
+    public void printExerciseHandlesEmptyRepsPerSet() {
+
+        Map<Integer, Integer> repsPerSet = new HashMap<>();
+        Exercise exercise = new Exercise("Bicep Curl", 4, repsPerSet, 12, 25.0, Mode.BARBELL);
+
+        // Redirect System.out to a ByteArrayOutputStream
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        exercise.printExercise();
+
+        // Reset System.out
+        System.setOut(System.out);
+
+        String expectedOutput = "Bicep Curl 4x12@25.0 (Reps per set: {})\n";
+        Assert.assertEquals(expectedOutput, outputStream.toString());
+    }
+
+    @Test
     public void updateFunctionsOnExerciseFailOnInvalidInput() {
         Map<Integer, Integer> repsPerSet = new HashMap<>();
         repsPerSet.put(0, 5);
@@ -84,5 +129,75 @@ public class ExerciseTest {
         Exercise e2 = new Exercise("Incline Bench Press", 4, repsPerSet, 8, 65.00, Mode.DUMBBELL);
 
         Assert.assertFalse((e1.equals(e2)));
+    }
+
+    @Test
+    public void equalOverrideReturnsFalseWhenNull(){
+        Map<Integer, Integer> repsPerSet = new HashMap<>();
+        Exercise e1 = new Exercise("Bench Press", 4, repsPerSet, 8, 65.00, Mode.DUMBBELL);
+        Exercise e2 = null;
+
+        Assert.assertFalse((e1.equals(e2)));
+    }
+
+    @Test
+    public void sameHashCodeForSameObject() {
+        Map<Integer, Integer> repsPerSet = new HashMap<>();
+        repsPerSet.put(0, 8);
+        repsPerSet.put(1, 8);
+        Exercise exercise = new Exercise("Shoulder Press", 4, repsPerSet, 8, 50.0, Mode.DUMBBELL);
+
+        int hashcode1 = exercise.hashcode();
+        int hashcode2 = exercise.hashcode();
+
+        Assert.assertEquals(hashcode1, hashcode2);
+    }
+
+    @Test
+    public void consistentHashCodeForEqualObjects() {
+
+        Map<Integer, Integer> repsPerSet1 = new HashMap<>();
+        repsPerSet1.put(0, 8);
+        repsPerSet1.put(1, 8);
+        Exercise exercise1 = new Exercise("Shoulder Press", 4, repsPerSet1, 8, 50.0, Mode.DUMBBELL);
+
+        Map<Integer, Integer> repsPerSet2 = new HashMap<>();
+        repsPerSet2.put(0, 8);
+        repsPerSet2.put(1, 8);
+        Exercise exercise2 = new Exercise("Shoulder Press", 4, repsPerSet2, 8, 50.0, Mode.DUMBBELL);
+
+        Assert.assertEquals(exercise1.hashcode(), exercise2.hashcode());
+    }
+
+    @Test
+    public void differentHashCodeForUnequalObjects() {
+
+        Map<Integer, Integer> repsPerSet1 = new HashMap<>();
+        repsPerSet1.put(0, 8);
+        repsPerSet1.put(1, 8);
+        Exercise exercise1 = new Exercise("Bench Press", 4, repsPerSet1, 8, 65.00, Mode.BARBELL);
+
+        Map<Integer, Integer> repsPerSet2 = new HashMap<>();
+        repsPerSet2.put(0, 8);
+        repsPerSet2.put(1, 8);
+        Exercise exercise2 = new Exercise("Shoulder Press", 4, repsPerSet2, 8, 50.0, Mode.DUMBBELL);
+
+        Assert.assertNotEquals(exercise1, exercise2);
+    }
+
+    // Checks whether hashcode implementation allows for object to work as key of hashmap.
+    @Test
+    public void hashCodeWorksInHashMap() {
+
+        Map<Integer, Integer> repsPerSet = new HashMap<>();
+        repsPerSet.put(0, 8);
+        repsPerSet.put(1, 8);
+        Exercise exercise = new Exercise("Shoulder Press", 4, repsPerSet, 8, 50.0, Mode.DUMBBELL);
+
+        HashMap<Exercise, String> map = new HashMap<>();
+        map.put(exercise, "Valid Exercise");
+
+        Assert.assertTrue(map.containsKey(exercise));
+        Assert.assertEquals("Valid Exercise", map.get(exercise));
     }
 }
